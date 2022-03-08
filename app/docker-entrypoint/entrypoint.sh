cfcccbffb6583ae3fc4af9d5d582871922551fba
nitishcm <nitishchandramathur@gmail.com>
2022-03-08 23:25:08 +0000

    working solution

--- app/docker-entrypoint/entrypoint.sh
+++ app/docker-entrypoint/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -jar /app/localstack-demo-0.0.1-SNAPSHOT.jar --test.data.file.path=/app/testData.json --amazon.dynamodb.endpoint=http://localstack:4566
+java -jar bin/localstack-demo-0.0.1-SNAPSHOT.jar --test.data.file.path=testData.json --amazon.dynamodb.endpoint=http://localstack:4566
